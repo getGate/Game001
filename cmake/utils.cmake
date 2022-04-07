dee1f597f9757c4dec0c7355ae2bd6df9df77d1b
dingli <dingli@bytedance.com>
2022-04-07 14:22:34 +0800

    refactor project structure

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -9,3 +9,53 @@ MACRO(SUBDIRLIST result curdir)
     ENDFOREACH()
     SET(${result} ${dirlist})
 ENDMACRO()
+
+MACRO(ADDGAME game sources resources)
+    message("game: " ${game})
+    SET(THE_GAME_DIR "${GAME_BIN_DIR}/bin")
+    message("set bin path: " "${THE_GAME_DIR}")
+
+    add_executable(${game} sources)
+    
+    set_target_properties( ${game}
+        PROPERTIES
+        RUNTIME_OUTPUT_DIRECTORY ${THE_GAME_DIR}
+    )
+
+    if(DEFINE resources)
+        add_custom_command(TARGET ${game}
+            POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy ${resources} ${THE_GAME_DIR}
+        )
+    endif(DEFINE resources)
+    
+ENDMACRO()
+
+MACRO(ADDGAMEDIR TARGET_DIR GAMES)
+
+    foreach(theGame ${GAMES})
+        if(EXISTS "${TARGET_DIR}/${theGame}/main.cpp")
+            message(STATUS "game: " ${theGame})
+            add_executable(${theGame} ${theGame}/main.cpp)
+
+            set(theBinDir "${GAME_BIN_DIR}/${theGame}")
+
+            message("game bin path: " "${theBinDir}")
+            set_target_properties( ${theGame}
+                PROPERTIES
+                RUNTIME_OUTPUT_DIRECTORY ${theBinDir}
+            )
+
+            # copy data-files if needed
+            set(theDataSrc "${TARGET_DIR}/${theGame}/data")
+            if(EXISTS "${theDataSrc}")
+            add_custom_command(TARGET ${theGame}
+                POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy ${theDataSrc} ${theBinDir}
+                DEPENDS ${theDataSrc}
+            )
+            endif(EXISTS "${theDataSrc}")
+            
+        endif(EXISTS "${TARGET_DIR}/${theGame}/main.cpp")
+    endforeach()
+ENDMACRO()
--- prune_source.sh
+++ prune_source.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 find . -name "*.vcxproj" -exec rm -rf {} \;
+find . -name "*.sln" -exec rm -rf {} \;
+find . -name ".vs" -exec rm -rf {} \;
